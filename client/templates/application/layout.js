5642311d93f79a2841fe46690e5500e744aa46c3
Tom <tmalone@protonmail.com>
2015-08-01 23:19:35 +0000

    Removed the layout.js helper file. It was only used in the DM book's Session chapter to demonstrate session functionality in Meteor.

--- client/templates/application/layout.js
+++
@@ -1,4 +0,0 @@
-Template.layout.helpers({
-  // Page heading/title of each section -- displayed in .page-header (i.e. not the actual <title></title> value)
-  // pageTitle: function() { return Session.get('pageTitle'); }
-});
