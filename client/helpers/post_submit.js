58e571173dcc49f1e0f657f82194f50c56051f43
Tom <tmalone@protonmail.com>
2015-08-02 03:05:27 +0000

    - Created Post submission template & form - Hooked up a route for it - Created a helper that processes

---
+++ client/helpers/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events({
+  'submit newPostForm': function(e) {
+    // Prevent form from being submitted over HTTP
+    e.preventDefault();
+    
+    var post = {
+      title:    $(e.target).find('[name=title]').val(),
+      url:      $(e.target).find('[name=url]').val()
+    };
+    
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
