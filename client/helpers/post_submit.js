a5caee292849e8a8365b186c0b74a16252a9478a
Tom <tmalone@protonmail.com>
2015-08-03 02:44:41 +0000

    Modified post submission validatio in postSubmit template's event handler to ensure that all new Posts have http/https protocol included.

--- client/helpers/post_submit.js
+++ client/helpers/post_submit.js
@@ -8,7 +8,17 @@ Template.postSubmit.events({
       url:      $(e.target).find('[name=url]').val()
     };
     
+    // Ensure URL begins with HTTP or Secure HTTP protocol
+    if ( ! urlHasProtocol(post.url)) {
+      post.url = 'http://' + post.url;
+    }  
+    
     post._id = Posts.insert(post);
     Router.go('postPage', post);
   }
 });
+
+function urlHasProtocol(url) {
+  var re = /^https?:\/\//i;
+  return re.test(url);
+}
