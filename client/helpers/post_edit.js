9cf71bc2f610caa939e1baf88649713e3df1db21
Tom Malone <tmalone+git@gmail.com>
2015-08-03 21:32:22 -0400

    Fixed (well, attempted to fix) problem with posts being deleted when .delete button is clicked

--- client/helpers/post_edit.js
+++ client/helpers/post_edit.js
@@ -2,14 +2,14 @@ Template.postEdit.helpers({
   'submit form': function(e) {
     // Prevent form from being submitted the default way, through HTTP
     e.preventDefault();
-    
+
     var currentPostId = this._id;
-    
+
     var postAttributes = {
       title:    $(e.target).find('[name=title]').val(),
       url:      $(e.target).find('[name=url]').val()
     };
-    
+
     Posts.update(currentPostId, {$set: postAttributes}, function(error) {
       if (error) {
         // Display error to user
@@ -20,14 +20,14 @@ Template.postEdit.helpers({
       }
     });
   },
-  
+
   'click .delete': function(e) {
     // Prevent a link from being followed by default
     e.preventDefault();
-    
+
     if ( confirm("Do you really want to delete this post?") ) {
       var currentPostId = this._id;
-      Posts.remove(currentPostId);
+      Posts.remove({_id: currentPostId});
       Router.go('postsList');
     }
   }
