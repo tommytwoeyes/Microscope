c7838643b9cc4e061e6d062e904d1bc09af0e4c8
Tom <tmalone@protonmail.com>
2015-08-04 01:07:28 +0000

    Moved postItem helper functions to own file. Added ownPost() function, which is used to determine if the current post (i.e. the post referenced by the post_item template) belongs to the currently logged-in Meteor user

---
+++ client/helpers/post_item.js
@@ -0,0 +1,13 @@
+Template.postItem.helpers({
+  ownPost: function() {
+    // Determine if the account used to created the current post 
+    // is the same account as the currently logged-in user
+    return this.userId === Meteor.userId();
+  },
+  
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
+});
