5a319919681a3acf068fbd3cb1b5f1450fcda2d7
Tom <tmalone@protonmail.com>
2015-08-05 00:23:34 +0000

    Added a global template helper that gets the name of the current route

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,8 +1,11 @@
 // Defines helper functions which can be called from any template, 
 // globally throughout the app
 
-Template.registerHelper('urlHasProtocol', function(url) {
+Template.registerHelper('urlHasProtocol', function(url) {    // Does the url include http:// or https://
   var re = /^https?:\/\//i;
   return re.test(url);
 });
 
+Template.registerHelper('currentRoute', function() {
+  return Router.current().route.getName();
+});
