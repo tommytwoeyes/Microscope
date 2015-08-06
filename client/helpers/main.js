ca522082eb9663bbcaf676447175659841e68a5e
Tom <tmalone@protonmail.com>
2015-08-06 01:10:52 +0000

    Deleted the file /client/helpers/main.js because files with 'main' in the filename are loaded last, and I want to ensure any application-wide template helpers are NOT loaded last. The app-wide template helpers are now in /client/helpers/application_helpers.js

--- client/helpers/main.js
+++
@@ -1,6 +0,0 @@
-// Defines helper functions which can be called from any template, 
-// globally throughout the app
-
-Template.registerHelper('currentRoute', function() {
-  return Router.current().route.getName();
-});
