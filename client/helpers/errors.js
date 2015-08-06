6fad24995d67ec5a495c92002496012f046b523f
Tom <tmalone@protonmail.com>
2015-08-06 01:01:21 +0000

    5 #close Fixed the issue with the .alert messages consuming space in DOM after fading out (actually, the book code contained a fix, in chapter 9). The solution was to use Meteor's native Reactivity to resolve the issue by removing the error message from Minimongo (client-side in-memory database simulation), which causes Reactivity to delete the corresponding DOM element from the template, from within the Template.onRendered() callback

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -11,3 +11,13 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+// Remove error messages after their CSS3 animation has completed,
+// so they don't accumulate in the DOM, taking up space in an 
+// undesirable way
+Template.errors.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function() { // Remove the .alert message from the DOM 0.2ms after it fades out
+    Errors.remove(error._id);
+  }, 5200);
+});
