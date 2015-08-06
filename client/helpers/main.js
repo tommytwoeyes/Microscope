ff783a1df6fe59417da2279361c35650d94f7758
Tom <tmalone@protonmail.com>
2015-08-06 00:37:28 +0000

    Created file in /lib (so its functions will be available both client-side and server-side) called validators.js and moved the 'urlHasProtocol()' function there. It had been registered as a global Template helper, but I was calling it from the event handler part of post_submit.js, and Meteor was throwing an error, saying the function was undefined. It makes more sense to have it defined in /lib/validators.js

--- client/helpers/main.js
+++ client/helpers/main.js
@@ -1,11 +1,6 @@
 // Defines helper functions which can be called from any template, 
 // globally throughout the app
 
-Template.registerHelper('urlHasProtocol', function(url) {    // Does the url include http:// or https://
-  var re = /^https?:\/\//i;
-  return re.test(url);
-});
-
 Template.registerHelper('currentRoute', function() {
   return Router.current().route.getName();
 });
