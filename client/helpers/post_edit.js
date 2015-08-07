d8d0868046a47a61aa1d468a5ab7b25296193cf1
Tom <tmalone@protonmail.com>
2015-08-07 00:25:17 +0000

    Added the errorMessage() and errorClass() validation helpers to the [ /client/helpers/post_edit.js ] helper file and the [ /client/templates/posts/post_edit.html ] template.

--- client/helpers/post_edit.js
+++ client/helpers/post_edit.js
@@ -1,3 +1,27 @@
+// Initialize the errorMessages object every time the postEdit
+// template is rendered, so that the user will never see old error 
+// messages from a previous interaction with the form
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+
+// These helpers generate the reactive error messages and
+// error message CSS classes that (reactively) get added to
+// and removed from the form as the user interacts with it
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  
+  errorClass: function(field) {
+    if ( Session.get('postEditErrors')[field] ) {
+      return 'has-error';
+    }
+    
+    return '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     // Prevent form from being submitted the default way, through HTTP
