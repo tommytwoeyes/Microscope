4ad3233f60fa091a37eebc98b52cb2873137e863
Tom <tmalone@protonmail.com>
2015-08-07 01:24:14 +0000

    Added some comment documentation for the Template.error.onRendered() callback

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -17,7 +17,12 @@ Template.errors.helpers({
 // undesirable way
 Template.error.onRendered(function() {
   var error = this.data;
-  Meteor.setTimeout(function() { // Remove the .alert message from the DOM 0.2ms after it fades out
+  
+  // Remove the .alert message from the DOM 0.2ms after it fades out
+  // If you don't do this, repeated errors stack up in the DOM; they're
+  // not visible, but they take up an increasing amount of space, pushing 
+  // the rest of your content down (looks bad, BAD)
+  Meteor.setTimeout(function() { 
     Errors.remove(error._id);
   }, 5050);
 });
