62131e2637c463dc3008e9c1c205eccc2fd884ff
Tom Malone <tmalone+git@gmail.com>
2015-08-10 23:48:31 -0400

    Created a form for comment submissions

--- packages/errors/errors_list.js
+++ packages/errors/errors_list.js
@@ -7,10 +7,10 @@ Template.meteorErrors.helpers({
 
 Template.meteorErrors.rendered = function() {
   var error = this.data;
-  
+
   // Remove the .alert message from the DOM 0.2ms after it fades out
   // If you don't do this, repeated errors stack up in the DOM; they're
-  // not visible, but they take up an increasing amount of space, pushing 
+  // not visible, but they take up an increasing amount of space, pushing
   // the rest of your content down (looks bad, BAD)
   Meteor.setTimeout(function() {
     Errors.collection.remove(error._id);
