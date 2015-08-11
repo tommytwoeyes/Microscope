89c4004d202019fcf53d7bd7d26d51cf868a84f8
Tom Malone <tmalone+git@gmail.com>
2015-08-10 23:45:40 -0400

    Created new file [ /client/helpers/comment_submit.js ] which: resets the errors for the comment form in Session, includes helpers for validating the comment form, and has an event handler which calls the validation helpers and submits the form when its data is valid

---
+++ client/helpers/comment_submit.js
@@ -0,0 +1,43 @@
+Template.commentSubmit.onCreated(function() {
+  Session.set('commentSubmitErrors', {});
+});
+
+Template.commentSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('commentSubmitErrors')[field];
+  },
+
+  errorClass: function (field) {
+    if ( Session.get('commentSubmitErrors')[field] ) {
+      return 'has-error';
+    }
+
+    return '';
+  }
+});
+
+Template.name.events({
+  'submit form': function(e, template){
+    e.preventDefault();
+
+    var $body = $(e.target).find('[name=body]');
+    var comment = {
+      body: $body.val(),
+      postId: template.data._id
+    };
+
+    var errors = {};
+    if( ! comment.body ) {
+      errors.body = 'Please write a comment',
+      return Session.set('commentSubmitErrors', errors);
+    }
+
+    Meteor.call('commentInsert', comment, function(error, commentId) {
+      if (error) {
+        throw(error.reason);
+      } else {
+        $body.val('');
+      }
+    });
+  }
+});
