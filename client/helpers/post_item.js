d5c4c8b4cde32113843b139ca512d1040e7ca645
Tom Malone <tmalone+git@gmail.com>
2015-08-12 22:29:28 -0400

    Removed no longer necessary commentsCount() helper; the Posts Collection document model now includes a commentsCount attribute in the data.

--- client/helpers/post_item.js
+++ client/helpers/post_item.js
@@ -9,9 +9,5 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  },
-
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count();
   }
 });
