9fa3e3638392ddb8797940b8adba46448b3d61ee
Tom Malone <tmalone+git@gmail.com>
2015-08-13 02:30:07 -0400

    Added a deploy() function to shortcut the series of commands needed to run [mup deploy] with a passphrase

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,19 @@
 
 from fabric.api import local
 
-project_dir = '/Users/tom/Projects/Meteor/Microscope'
+project_dir		= '/Users/tom/Projects/Meteor/Microscope'
+
 
 def commit_pull_push_all(msg):
-    local("cd " + project_dir)
-    local('git commit -am "' + msg + '"')
-    local('git pull github master')
-    local('git pull bitbucket master')
-    local('git push all master')
+	local("cd " + project_dir)
+	local('git commit -am "' + msg + '"')
+	local('git pull github master')
+	local('git pull bitbucket master')
+	local('git push all master')
+
+
+def deploy(key_path):
+	local("cd {0}/.deploy".format(project_dir))
+	local('eval $(ssh-agent)')
+	local("ssh-add {0}".format(key_path))
+	local("mup deploy")
