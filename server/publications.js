e690c80acbf898a0550e753cd939c20fa6b7d9c6
Tom Malone <tmalone+git@gmail.com>
2015-08-12 21:26:11 -0400

    Moved the code that filters Post documents by $title $regex query to a named function definition. Also, added comment docs. Also, in comment docs, added @todo item for defining actual categories of posts. This way, when I write additional filters, the publications code will be more organized.

--- server/publications.js
+++ server/publications.js
@@ -1,13 +1,28 @@
-Meteor.publish('posts', function (searchString){
-  check(searchString, String);
-  
-  return Posts.find({
-    title:    {
-      $regex:    searchString,
-      $options:  'i'
-    }
-  });
-});
+/**
+ * Filters Posts by title
+ *
+ * @todo Replace this functionality with actual categories:
+ * - i.e.:
+ *   - Create Category collection
+ *   - Add CategorId to Post objects
+ *   - Write filterByCategory() function
+ *   - Modify publication and subscription accordingly
+ *
+ * @param   {String} searchString The specific text to find in title
+ * @returns {Object} Posts matching [title] query
+ */
+var filterByTitle = function filterByTitle(searchString) {
+	check(searchString, String);
+
+	return Posts.find({
+		title: {
+			$regex:			searchString,
+			$options:		'i'
+		}
+	});
+};
+
+Meteor.publish('posts', filterByTitle(searchString));
 
 Meteor.publish('comments', function() {
   return Comments.find();
