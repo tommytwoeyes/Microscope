4256573b1b15c4e318a0fa9c6d689cec6ea1b172
Tom Malone <tmalone+git@gmail.com>
2015-08-13 05:19:28 -0400

    Added comment count for current post at top of comment list

--- client/helpers/comment_list.js
+++ client/helpers/comment_list.js
@@ -2,4 +2,8 @@ Template.commentList.helpers({
   comments: function() {
     return Comments.find({postId: this.postId});
   },
+
+	currentCount: function() {
+		return Posts.findOne(this.postId).commentsCount;
+	}
 });
