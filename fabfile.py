678f557b6282c59015b9d3e12bc164590e9bb7b9
Tom Malone <tmalone+git@gmail.com>
2015-08-18 22:42:47 -0400

    Refactored Fabric file to make it more pythonic (using str.format() ) and less error prone (by removing hard-coded git branch and remote names and replacing them with variables).

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python
 
-from fabric.api import local
+"""
+This is a Fabric file [see http://www.fabfile.org/ for details].
+Fabric is a Python command-line utility for automating tedious
+deployment/build tasks, etc.
 
-project_dir		= '/Users/tom/Projects/Meteor/Microscope'
+The purpose of this file is to remove some of the tedium of the
+Git workflow for me in this project.
+"""
 
+from fabric.api import *
 
-def commit_pull_push_all(msg):
-	local("cd " + project_dir)
-	local('git commit -am "' + msg + '"')
-	local('git pull github master')
-	local('git pull bitbucket master')
-	local('git push all master')
+# Project configuration
+project_dir							= '/Users/tom/Projects/Meteor/Microscope'
+git_remote_bitbucket 		= 'bitbucket'
+git_remote_github 			= 'github'
+git_remote_combined 		= 'all'
+
+def commit_pull_push(msg, branch='master'):
+	"""
+	Performs the following Git workflow:
+	- Commits current state with message
+	- Performs a `git pull`
+	- Performs a `git push`
+
+	Args:
+		msg 		(str)	The commit message
+		branch		(Optional[str]) The branch to pull/push to
+	"""
+
+	# Escape any quotes, newlines or other special chars in the commit message
+	commit_cmd = """git commit -am '{0}'""".format(msg)
+	pull_cmd_first = "git pull {0} {1}".format(git_remote_github, branch)
+	pull_cmd_second = "git pull {0} {1}".format(git_remote_bitbucket, branch)
+	push_cmd = "git push {0} {1}".format(git_remote, branch)
+
+	local( "cd {0}".format(project_dir) )
+	local( commit_cmd )
+	local( pull_cmd_first )
+	local( pull_cmd_second )
+	local( push_cmd )
 
 
 def deploy(key_path):
